0f73d51cb8a70744d04c62bec7b916dcb360346b
AAkinshin <rdr-rdr@yandex.ru>
2021-01-07 06:36:09 +0300

    - edit app js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const appDiv = document.querySelector('#app');
-const helloDiv = document.createElement('div');
-helloDiv.className = 'container';
-helloDiv.innerText = 'Hello!';
-appDiv.appendChild(helloDiv);
+const ulElement = document.querySelector('.buttons');
+const resultElement = document.querySelector('.result');
+
+for (let i = 0; i < 10; i++) {
+    let btnElement = document.createElement('button');
+
+    btnElement.textContent = `Я номер ${i + 1}`;
+    btnElement.classList.add('btn', 'btn-primary');
+    ulElement.appendChild(btnElement);
+
+    btnElement.addEventListener('click', () => {
+        if(i === 4) {
+            alert('You are lucky');
+            return true;
+        }
+
+        resultElement.textContent = `Ты нажал на кнопку с номером: ${i + 1}`;
+    });
+}
+   
+ 
